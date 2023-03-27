90043ab85f0d9835ebc8a4801330342f2aad417e
harbee7 <abigailjegede38@gmail.com>
2023-03-27 04:21:11 -0700

    Done with task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ * @str: the string
+ * Return: lenght of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		putchar(str[a]);
+		a++;
+	}
+	putchar('\n");
+}
