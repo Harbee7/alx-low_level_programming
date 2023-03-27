eeadd05fd4632a8698137602c0d981d34444a41d
harbee7 <abigailjegede38@gmail.com>
2023-03-27 12:05:43 -0700

    Done with task 8

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -14,6 +15,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
-int printf(const char *, ...);
 
 #endif
