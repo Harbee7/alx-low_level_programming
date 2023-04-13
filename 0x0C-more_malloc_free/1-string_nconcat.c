28d0270affc5c1022f9af272c97719722daff6cf
harbee7 <abigailjegede38@gmail.com>
2023-04-13 04:53:52 -0700

    Done with task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int k = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -22,25 +22,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
-
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (!s)
 		return (NULL);
-	while (k < len1)
+	while (i < len1)
 	{
-		s[k] = s1[k];
-		k++;
+		s[i] = s1[i];
+		i++;
 	}
 
-	while (n < len2 && k < (len1 + n))
-		s[k++] = s2[j++];
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	while (n >= len2 && k < (len1 + len2))
-		s[k++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	s[j] = '\0';
+	s[i] = '\0';
 
 	return (s);
 }
