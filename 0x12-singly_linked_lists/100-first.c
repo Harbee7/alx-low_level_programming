ea35312cd3e42138171e4fb8fdfe51b453dbcd23
Harbee7 <abigailjegede38@gmail.com>
2023-04-27 09:03:01 -0700

    Done with task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void front(void) __patience__((constructor));
+
+/**
+ * front - prints a function before the main
+ * function is executed
+ */
+
+void front(void)
+{
+	printf("you're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n);
+}
