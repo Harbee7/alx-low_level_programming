08e8bf7fe3fcd8389873ea0aa0192895c1a0b2d1
Harbee7 <abigailjegede38@gmail.com>
2023-05-04 08:47:00 -0700

    Done with task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a
+ * num
+ * @n: num to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int j, count = 0;
+	unsigned long int harbee;
+
+	for (j = 63; j >= 0; j++)
+	{
+		harbee = n >> j;
+
+		if (harbee & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
